097c5ddd2a3522c57300453d6ffae6ffaa61f5c8
khcatino <kcatino@itba.edu.ar>
2020-12-06 11:47:14 -0300

    comentarios del .h

--- Q1ADT.h
+++ Q1ADT.h
@@ -1,20 +1,26 @@
 #ifndef Q1ADT_H_INCLUDED
 #define Q1ADT_H_INCLUDED
 
+typedef struct Q1CDT * Q1ADT;
 
+/*Structure used to return in array. "hood" is a string that contains the name of the neighborhood.
+"density" is a double that stores the density of trees/population for a given neighborhood*/
 typedef struct {
     char * hood;
     double density;
 }TQ1;
 
-typedef struct Q1CDT * Q1ADT;
-
+/*Creates structure and returns pointer to it*/
 Q1ADT newQ1struct ();
 
+/* Adds neighborhood name and density to the ADT. Returns 1 if the neighborhood is added correctly.
+If an issue is encountered while allocating memory for it, the return value is 0*/
 int addQ1hood(Q1ADT adt, char * hood, double density);
 
+/*Returns array of TQ1 structures that is in descending density order. If two neighborhoods have the same density, the order is in ascending alphabetical order*/
 TQ1 * solveQ1(Q1ADT adt, int *dim);
 
+/*Frees all allocated memory utilized to create and maintain the abstract data type*/
 void freeQ1 (Q1ADT adt);
 
 
--- arbolesADT.h
+++ arbolesADT.h
@@ -2,12 +2,16 @@
 #define ARBOLESADT_H_INCLUDED
 
 typedef struct arbolesCDT * arbolesADT;
-
+/*Generic structure that stores a string and an integer*/
 typedef struct {
     char * name;
     int tQty;
 }TreeStreet;
 
+/*Structure that will be returned in the form of an array in following functions. "hood" is a string that stores the name of the neighborhood. The following
+variables store a value related to the given neighborhood represented by the string "hood".
+"popTree" stores the most popular tree name and the quantity. "popStreet" stores the street with the most trees.
+"pQty" is an integer that stores the population and "tQty" stores the total amount of trees.*/
 typedef struct {
     char * hood;
     TreeStreet popTree;
@@ -16,22 +20,39 @@ typedef struct {
     int tQty;
 }TQ23;
 
+/*Structure that will be returned in the form of an array in following functions. "tree" is a string that stores the name of the
+species of a given tree. The following variables store a value related to the tree species represented by "tree".
+"dMin" stores the minimum diameter recorded. "dMax" stores the maximum diameter recorded.*/
 typedef struct {
     char * tree;
     double dMin;
     double dMax;
 }TQ4;
 
+/*Returns a pointer to the abstract data type*/
 arbolesADT newStruct ();
 
+/* Adds neighborhood name and population to the ADT. Returns 1 if the neighborhood is added correctly.
+If an issue is encountered while allocating memory for it, the return value is 0*/
 int addHood (arbolesADT adt, char * hood, int pQty);
 
+/* Adds tree species to the ADT. Returns 1 if the all values are added correctly.
+If an issue is encountered while allocating memory for it, the return value is 0*/
 int addTree (arbolesADT adt, char * hood, char * street, char * tree, double diam);
 
+/*Returns an array of TQ23 structures that is ordered by neighborhood names in alphabetical order.
+The dimension of the array is stored in *dim.
+If the array is generated correctly, the pointer returned has a value different from NULL.
+If an error is encountered while allocating memory for it, the return value is NULL*/
 TQ23 * solveQ23(arbolesADT adt, int * dim);
 
+/*Returns an array of TQ4 structures that is ordered by the name of tree species in alphabetical order.
+The dimension of the array is stored in *dim.
+If the array is generated correctly, the pointer returned has a value different from NULL.
+If an error is encountered while allocating memory for it, the return value is NULL*/
 TQ4 * solveQ4(arbolesADT adt, int * dim);
 
+/*Frees all allocated memory utilized to create and maintain the abstract data type*/
 void freeADT (arbolesADT adt);
 
 
